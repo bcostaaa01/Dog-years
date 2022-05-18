197e47eea05a976631b84332f098b59baf66f982
Bruno <72168158+bcostaaa01@users.noreply.github.com>
2022-05-18 13:37:22 +0100

    Create dog-years.js
    Add codebase to repo

---
+++ dog-years.js
@@ -0,0 +1,14 @@
+const myAge = 20 // my age
+
+let earlyYears = 2 // early dog life years
+
+earlyYears *= 10.5
+
+let laterYears = myAge - 2
+laterYears *= 4
+
+myAgeInDogYears = earlyYears + laterYears
+
+let myName = "Bruno".toLowerCase()
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
